beee208823f7fbb413021fa6fe893366c0caf94f
fukiame <fukiame@proton.me>
2026-03-02 01:41:59 +0700

    build: drop unused ro build ver prop
    Signed-off-by: fukiame <fukiame@proton.me>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,5 +6,4 @@ endif
 
 # LineageOS System Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
+    ro.lineage.version=$(LINEAGE_VERSION)
