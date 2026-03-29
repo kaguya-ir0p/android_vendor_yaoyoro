d5e9a1c4611a41b86c3b3dc7f5cff5e96569b71e
fukiame <fukiame@proton.me>
2026-03-29 12:41:24 +0700

    build: pancake
    Signed-off-by: fukiame <fukiame@proton.me>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -20,9 +20,13 @@ LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/yaoyoro-$(LINEAGE_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
-.PHONY: bacon
-bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
+.PHONY: bacon pancake
+pancake: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
 	$(hide) rm -rf $(call intermediates-dir-for,PACKAGING,target_files)
 	@echo "package complete ~ $(LINEAGE_TARGET_PACKAGE)" >&2
+	@echo "package sha256: `cat $(LINEAGE_TARGET_PACKAGE).sha256sum | cut -d ' ' -f 1`" >&2
+	@echo "パンケーキ食べよ？" >&2
+
+bacon: pancake
